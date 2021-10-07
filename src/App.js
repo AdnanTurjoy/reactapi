191011764d6f0c725844a0ff18503cd6cbd0c9a6
adnan <aturjoy4@gmail.com>
2021-10-08 00:45:57 +0600

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 import {Button} from 'react-bootstrap';
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Header from './Header';
+import Login from './Login';
+import AddProduct from './AddProduct';
+import UpdateProduct from './UpdateProduct';
+import Register from './Register';
 
 
 function App() {
   return (
     <div className="App">
-      <button className="btn btn-danger">Normal</button>
-      <Button className="btn btn-danger">Boot</Button>
+      <BrowserRouter>
+      <Header/>
+      <h1>E-commerce</h1>
+      <Route path='/login'>
+         <Login/>
+      </Route>
+      <Route path='/addProduct'>
+         <AddProduct/>
+      </Route>
+      <Route path='/updateProduct'>
+         <UpdateProduct/>
+      </Route>
+      <Route path='/Register'>
+         <Register/>
+      </Route>
+      </BrowserRouter>
+      
     </div>
   );
 }
---
+++ src/UpdateProduct.js
@@ -0,0 +1,7 @@
+const UpdateProduct = () => {
+    return ( <div>
+        <h1>UpdateProduct</h1>
+    </div> );
+}
+ 
+export default UpdateProduct;
