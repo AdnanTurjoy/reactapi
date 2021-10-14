dedc4910ddc21d3627f28d137ea498aaa756e9eb
adnan <aturjoy4@gmail.com>
2021-10-15 02:08:29 +0600

    update product final,(key point: useHistory,defaultValue,)

--- src/UpdateProduct.js
+++ src/UpdateProduct.js
@@ -1,33 +1,88 @@
-import { useState, useEffect} from "react";
-import {Button} from "react-bootstrap"
-import { Link ,useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Button } from "react-bootstrap";
+import { Link, useParams,useHistory  } from "react-router-dom";
 const UpdateProduct = () => {
-    const [data,setData] =useState([]);
-    let { id } = useParams();
-    console.log(id);
-    useEffect( async () => {
-        getsingleData();
-      }, []);
-      const getsingleData = async ()=>{
-        const response = await fetch('http://127.0.0.1:8000/api/product/'+id);
-        const items = await response.json();
-        setData(items);
+    let history = useHistory();
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [price, setPrice] = useState("");
+  const [file, setFile] = useState("");
+  const [data, setData] = useState([]);
+  let { id } = useParams();
+  console.log(id);
+  useEffect(async () => {
+    getsingleData();
+  }, []);
+  const getsingleData = async () => {
+    const response = await fetch("http://127.0.0.1:8000/api/product/" + id);
+    const items = await response.json();
+    setData(items);
+    setName(items.name);
+    setDescription(items.description);
+    setPrice(items.price);
+    setFile(items.file);
+  };
+  const handleUpdate = async (id) => {
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("file", file);
+    formData.append("description", description);
+    formData.append("price", price);
+
+    await fetch(
+      "http://localhost:8000/api/update/" + id + "?_method=PUT",
+      {
+        method: "POST",
+        body: formData,
       }
-      const updateProduct =()=>{
-          
-      }
-    return ( <div className="col-sm-6 offset-sm-3">
-        <h1>Update Product</h1>
-        <label>Name</label>
-        <input className="form-control" type="text" value={data.name}/> <br/>
-        <label>Description</label>
-        <input className="form-control" type="text" value={data.description}/><br/>
-        <label>price</label>
-        <input className="form-control" type="text" value={data.price}/><br/>
-        <img style={{width: "120px",
-    height: "120px"}} src={"http://127.0.0.1:8000/images/"+data.file_path}/><br/>
-    <Button onClick={handleUpdate} variant="success">Update</Button>
-    </div> );
-}
- 
+    );
+    alert("Product updated successfully");
+    history.push('/product')
+  };
+  return (
+    <div className="col-sm-6 offset-sm-3">
+      <h1>Update Product</h1>
+      <label>Name</label>
+      <input
+        className="form-control"
+        type="text"
+        defaultValue={data.name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <br />
+      <label>Description</label>
+      <input
+        className="form-control"
+        type="text"
+        defaultValue={data.description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <br />
+      <label>price</label>
+      <input
+        className="form-control"
+        type="text"
+        defaultValue={data.price}
+        onChange={(e) => setPrice(e.target.value)}
+      />
+      <br />
+      <img
+        style={{ width: "120px", height: "120px" }}
+        src={"http://127.0.0.1:8000/images/" + data.file_path}
+      />
+      <br />
+      <input
+        type="file"
+        className="form-control"
+        name={file}
+        onChange={(e) => setFile(e.target.files[0])}
+      />
+      <Button onClick={() => handleUpdate(data.id)} variant="success">
+        Update
+      </Button>
+     
+    </div>
+  );
+};
+
 export default UpdateProduct;
